225aeb618f42fc9173ee20500659c6a2a2ba40bd
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-06-20 22:54:30 +0100

    Autocomplete

--- src/components/AutocompleteAC.tsx
+++ src/components/AutocompleteAC.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from '@emotion/serialize';
 import React, { useEffect, useState } from 'react';
 
 interface IProps {
@@ -5,25 +6,33 @@ interface IProps {
 }
 
 export default function AutocompleteAC({ options }: IProps) {
+  const [userInput, setUserInput] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
 
-  const [ userInput, setUserInput ] = useState("");
-
-  const handleChange = (e:any) => {
+  const handleChange = (e: any) => {
     // console.log("Change:", e);
-    setUserInput(e.target.value)
-  }
+    setUserInput(e.target.value);
+  };
 
   useEffect(() => {
-    console.log("userInput:", userInput);
-    
-  }, [userInput])
+    // console.log("userInput:", userInput);
+    setSuggestions(makeSuggestions(userInput, options, 5))
+  }, [userInput]);
+
+  const suggestionStyles:any = {
+    display: 'flex',
+    'flexDirection': 'column'
+  };
 
   return (
     <div className="autocomplete-ac__wrapper">
-      <div className="autocomplete-ac__suggestion-abs-wrapper">
-        <div className="autocomplete-ac__suggestion">Wolfgang Amadeus Mozart</div>
-        <div className="autocomplete-ac__suggestion">Alvin James Cousins</div>
-        <div className="autocomplete-ac__suggestion">Alvin James Cousins</div>
+      <div
+        style={userInput.length && suggestions.length ? suggestionStyles : {}}
+        className="autocomplete-ac__suggestion-abs-wrapper"
+      >
+        {suggestions.map((suggestion) => {
+          return <Suggestion text={suggestion} />
+        })}
       </div>
       <input
         onChange={handleChange}
@@ -34,4 +43,30 @@ export default function AutocompleteAC({ options }: IProps) {
       />
     </div>
   );
+}
+
+interface SProps {
+  text: string;
+}
+
+const Suggestion = ({ text }: SProps) => {
+  return <div className="autocomplete-ac__suggestion">{text}</div>;
+};
+
+function makeSuggestions(userInput: string, options: { name: string }[], limit?: number): string[] {
+
+  const textRef = userInput.toLowerCase();
+
+  let arr = options.map((option) => {
+    return option.name
+  }).filter((option:string) => {
+    return option.toLowerCase().includes(textRef)
+  })
+
+  if (limit && arr.length > 6) {
+    arr = arr.slice(0, limit)
+  }
+
+  return arr
+  
 }
