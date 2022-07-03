624a9183bc80b8ebbb3c26e0cf50075d6fa0a365
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-07-03 17:58:59 +0100

    Increment version

--- src/data/appConsts.ts
+++ src/data/appConsts.ts
@@ -1,6 +1,6 @@
 import { condensedDate } from '../components/Game/gameHelpers';
 
-export const VERSION = 1.35;
+export const VERSION = 1.40;
 export const FIRST_GAME = '220617';
 // export const GAMEDAY_ID = condensedDate();
 export const GAMEDAY_ID = '220607';
