c2306e86724aa543e6f67d6b9fbb837e507051ad
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-07-09 23:52:40 +0100

    Prepare for AWS deploy

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/bachle/static', express.static(path.join(__dirname, '../client/build', 'static')));
+app.use('/bachle', express.static(path.join(__dirname, '../client', 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
