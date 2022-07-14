718fa8e64a6261b5f12f2a75a1687437e93ad803
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-07-14 20:57:09 +0100

    Deploy scripts

--- user_data_script.sh
+++ user_data_script.sh
@@ -3,12 +3,17 @@
 # download nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+# source nvm
+. /.nvm/nvm.sh
+
+# install node
+nvm install 16
+
 # export NVM dir
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install 16
 
 # upgrade yum
 sudo yum upgrade
@@ -23,6 +28,8 @@ git clone https://github.com/ajcousins/bachle.git
 cd bachle
 
 npm install pm2 -g
+
+sudo chmod -R 755 .
 
 npm run install-server
 npm run install-client
