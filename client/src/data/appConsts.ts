72ffab34a4b0573d57959eee773100d75d12720b
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-07-24 22:00:34 +0100

    Composer names clean up

--- client/src/data/appConsts.ts
+++ client/src/data/appConsts.ts
@@ -3,4 +3,4 @@ import { condensedDate } from '../components/Game/gameHelpers';
 export const VERSION = 1.41;
 export const FIRST_GAME = '220717';
 export const GAMEDAY_ID = condensedDate();
-// export const GAMEDAY_ID = '220710';
+// export const GAMEDAY_ID = '220727';
