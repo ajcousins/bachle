10909a7f2de493552c575d523dbd7563190fe03a
Alvin Cousins <alvin.cousins@roh.org.uk>
2022-08-20 18:00:25 +0100

    Refresh button on countdown complete

--- client/src/components/Countdown.tsx
+++ client/src/components/Countdown.tsx
@@ -6,17 +6,28 @@ import {
 
 export default function Countdown() {
   const [secs, setSecs] = useState(getSecondsToTomorrow());
+  const [isAvailable, setIsAvailable] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setSecs(getSecondsToTomorrow());
+      const secs = getSecondsToTomorrow();
+      if (secs === 0) {
+        setIsAvailable(true);
+      }
+      setSecs(secs);
     }, 200);
     return () => {
       clearInterval(interval);
     };
   });
 
-  return (
+  return isAvailable ? (
+    <div className="countdown">
+      <button className="btn-full" onClick={() => window.location.reload()}>
+        Play New Game!
+      </button>
+    </div>
+  ) : (
     <div className="countdown">
       The next Bachle will be available in...
       <h2>{secsToClockFormat(secs)}</h2>
